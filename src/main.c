b290c125cbdf3e652246e66427cab5d15bf99c6f
Radisa Mitrovic <radisa05mitrovic.net@gmail.com>
2020-09-24 02:29:37 +0200

    dodate teksture, omogucen ispis tekuceg rezultata

--- src/main.c
+++ src/main.c
@@ -1,9 +1,18 @@
 #include <GL/glut.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include <math.h>
+#include "image.h"
 #define TIMER_ID 0
+
+/* Makroi za imena fajlova sa teksturama. */
+#define ZID "../Teksture/zid.bmp"
+#define VODA "../Teksture/voda.bmp"
+
+/* Identifikatori tekstura. */
+static GLuint names[2];
 
 /* Struktura koja predstavlja jednu prepreku. */ 
 typedef struct
@@ -28,7 +37,6 @@ static int end = 0;
 static int restart = 0;
 
 /* Pomocne vrednosti prilikom kretanja loptice. */
-static float ugao = 0;
 static float brzina_lopte = 0.35;
 static int ulevo  = 0 ;
 static int udesno  = 0;
@@ -43,13 +51,19 @@ static void on_keyboard(unsigned char key, int x, int y);
 static void on_release(unsigned char key, int x, int y);
 static void move_objects();
 
-/* Dimenzije ravni po kojima ide loptica . */
+/* Dimenzije ravni po kojima ide loptica. */
 static float x_plane = 10;
 static float y_plane = 1;
 static float z_plane = 50;
 static float x_plane2 = 10;
 static float y_plane2 = 1;
 static float z_plane2 = 150;
+
+/* z koordinate ravni levo i desno od glavne ravni(dodate zbog tekstura). */
+static float z_coord_left_first = 50;
+static float z_coord_right_first = 50;
+static float z_coord_left_second = 150;
+static float z_coord_right_second = 150;
 
 /* Duzina staze. */
 static float duzina_staze = 100;
@@ -66,7 +80,7 @@ float ugao_rotacije = 0;
 static int score = 0;
 
 /* Faktor ubrzanja loptice. */
-static float faktor_ubrzanja = 1.0001;
+static float faktor_ubrzanja = 1.0005;
 
 /* Pomocne funkcije za iscrtavanje delova scene. */
 static void nacrtaj_ravan();
@@ -82,6 +96,13 @@ static void upit_preseka();
 /* Pomocna funkcija  za resetovanje parametara. */
 static void postavi_pocetne_vrednosti();
 
+/* Funkcija za posatavljanje tekstura. */
+static void postavi_teksture();
+
+/* Funkcija koja prikazuje tekuci rezultat. */
+static void  prikazi_tekuci_rezultat();
+
+
 int main(int argc, char **argv)
 {
     /* Inicijalizuje se GLUT. */
@@ -89,19 +110,23 @@ int main(int argc, char **argv)
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 
     /* Kreira se prozor. */
-    glutInitWindowSize(500, 500);
+    glutInitWindowSize(1100, 800);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Zig Zag");
-
-    /* Registruju se callback funkcije. */    
+    
     srand(time(NULL));
+
+    /* Postavljaju se teksture. */
+    postavi_teksture();
+    
+    /* Registruju se callback funkcije. */    
     glutDisplayFunc(on_display);
     glutKeyboardFunc(on_keyboard);
     glutReshapeFunc(on_reshape);
     glutKeyboardUpFunc(on_release);
     
     /* Obavlja se OpenGL inicijalizacija. */
-    glClearColor(0.75, 0.75, 0.75, 0);
+    glClearColor(0, 0, 0.2, 0);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHTING);
 
@@ -132,7 +157,7 @@ static void on_display(void)
     gluLookAt(x_coord, y_coord + 5, z_coord - 5, x_coord, y_coord, z_coord + 7, 0, 1, 0);
 
     /* Postavlja se osvetljenje. */
-    GLfloat light_position[] = {0, 1, -0.3, 0};
+    GLfloat light_position[] = {0, 1, 0, 0};
     GLfloat light_ambient[] = {0.5, 0.6, 0.5, 1.0};
     GLfloat light_diffuse[] = {0.8, 0.8, 0.9, 1};
     GLfloat light_specular[] = {1, 1, 1, 1};
@@ -143,22 +168,8 @@ static void on_display(void)
     glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
     glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
     
-    
-    /* Iscrtavanje koordinatnog sistema. */
-    glBegin(GL_LINES);
-        // x osa -> plava
-        glColor3f(0, 0, 1);
-        glVertex3f(-1000, 0, 0);
-        glVertex3f(1000, 0, 0);
-        // y osa -> zelena
-        glColor3f(0, 1, 0);
-        glVertex3f(0, 0, 0);
-        glVertex3f(0, 1000, 0);
-        // z osa -> crvena
-        glColor3f(1, 0, 0);
-        glVertex3f(0, 0, -1000);
-        glVertex3f(0, 0, 1000);
-    glEnd();
+    /* U svakom koraku prikazujem tekuci rezultat. */
+    prikazi_tekuci_rezultat();
     
     /* Iscrtava se staza. */
     nacrtaj_ravan();
@@ -172,11 +183,15 @@ static void on_display(void)
         postavi_prepreke(2);
         prva_prepreka= 0;
     }
+    else
+    {
+        
     
     /* Iscrtavam prepreke. */
     nacrtaj_prepreke(1);
     nacrtaj_prepreke(2);
-
+    }
+    /* Nova slika se salje na ekran. */
     glutSwapBuffers();
 }
 
@@ -193,7 +208,7 @@ static void on_keyboard(unsigned char key, int x, int y){
         if (!start && !end)
         {
             /* Podesava se pozadina i poziva se on_display funkcija. */
-            glClearColor(0.75, 0.75, 0.75, 0);
+            glClearColor(0, 0, 0.2, 0);
             glutDisplayFunc(on_display);
             
             /* Funkcija koja translira loptu. */
@@ -211,7 +226,7 @@ static void on_keyboard(unsigned char key, int x, int y){
         break;
     case 'd':
     case 'D':
-        /* Pomeranje udesno, azurira se indikator za pomeranje ulevo. */
+        /* Pomeranje udesno, azurira se indikator za pomeranje udesno. */
         udesno = 1;
         glutPostRedisplay();
         break;
@@ -246,57 +261,177 @@ static void on_release(unsigned char key, int x, int y){
     }
 }
 
-/* Funkcija koja crta 2 ravni po kojima ide lopta. */
+/* Funkcija koja crta 2 ravni po kojima ide lopta, kao i ravni sa njihove strane. */
 static void nacrtaj_ravan(){
     
     /* Postavljanje prve ravni. */
     glPushMatrix();
+    
+    /*Dodavanje tekstura na prvu ravan. */
+    glBindTexture(GL_TEXTURE_2D, names[0]);
+    glEnable(GL_TEXTURE_2D);
+    
     glBegin(GL_QUAD_STRIP);
     glNormal3f(0, 1, 0);
     int first = 0;
     int second = 0;
     for (int i = 0; i <= 5; i++)
     {
+        glTexCoord2f(first, second);
         first = (first + 1) % 2;
         glVertex3f(-5, -0.2, z_plane - 50 + i * 20);
+        
+        glTexCoord2f(first, second);
         first = (first + 1) % 2;
         second = (second + 1) % 2;
         glVertex3f(5, -0.2, z_plane - 50 + i * 20);
-    }
+    }   
     glEnd();
-
-    glTranslatef(0, -y_plane / 2, z_plane);
-    glScalef(x_plane, y_plane / 2, duzina_staze);
-    glutSolidCube(1);
+    glDisable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, 0);
     glPopMatrix();
 
     /* Postavljanje druge ravni. */
     glPushMatrix();
+    
+    /*Dodavanje teksture na drugu ravan. */
+    glBindTexture(GL_TEXTURE_2D, names[0]);
+    glEnable(GL_TEXTURE_2D);
     glBegin(GL_QUAD_STRIP);
     glNormal3f(0, 1, 0);
     first = 0;
     second = 0;
     for (int i = 0; i <= 5; i++)
     {
+        glTexCoord2f(first, second);
         first = (first + 1) % 2;
         glVertex3f(-5, -0.2, z_plane2 - 50 + i * 20);
+        glTexCoord2f(first, second);
         first = (first + 1) % 2;
         second = (second + 1) % 2;
         glVertex3f(5, -0.2, z_plane2 - 50 + i * 20);
     }
     glEnd();
-    glTranslatef(0, -y_plane2 / 2, z_plane2);
-    glScalef(x_plane2, y_plane2 / 2, duzina_staze);
-    glutSolidCube(1);
+    glDisable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    
+    // Prva ravan desno od glavne i njene teksture. */
+    glPushMatrix();
+    glBindTexture(GL_TEXTURE_2D, names[1]);
+    glEnable(GL_TEXTURE_2D);
+
+    glBegin(GL_QUAD_STRIP);
+    glNormal3f(0, 1, 0);
+    first = 0;
+    second = 0;
+    for (int i = 0; i <= 5; i++)
+    {
+        glTexCoord2f(first, second);
+        first = (first + 1) % 2;
+        glVertex3f(-5, -0.2, z_coord_right_first - 50 + i * 20);
+
+        glTexCoord2f(first, second);
+        first = (first + 1) % 2;
+        second = (second + 1) % 2;
+        glVertex3f(-35, -0.2, z_coord_right_first - 50 + i * 20);
+    }
+
+    glEnd();
+
+    glDisable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glPopMatrix();
+    
+    /*Druga ravan desno od glavne i njene teksture. */
+    glPushMatrix();
+    glBindTexture(GL_TEXTURE_2D, names[1]);
+    glEnable(GL_TEXTURE_2D);
+
+    glBegin(GL_QUAD_STRIP);
+    glNormal3f(0, 1, 0);
+    first = 0;
+    second = 0;
+    for (int i = 0; i <= 5; i++)
+    {
+        glTexCoord2f(first, second);
+        first = (first + 1) % 2;
+        glVertex3f(-5, -0.2, z_coord_right_second - 50 + i * 20);
+
+        glTexCoord2f(first, second);
+        first = (first + 1) % 2;
+        second = (second + 1) % 2;
+        glVertex3f(-35, -0.2, z_coord_right_second - 50 + i * 20);
+    }
+
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glPopMatrix();
+
+    /* Prva ravan levo od glavne i njene teksture. */
+    glPushMatrix();
+    glBindTexture(GL_TEXTURE_2D, names[1]);
+    glEnable(GL_TEXTURE_2D);
+        
+    glBegin(GL_QUAD_STRIP);
+    glNormal3f(0, 1, 0);
+    first = 0;
+    second = 0;
+    for (int i = 0; i <= 5; i++)
+    {
+        glTexCoord2f(first, second);
+        first = (first + 1) % 2;
+        glVertex3f(5, -0.2, z_coord_left_first - 50 + i * 20);
+
+        glTexCoord2f(first, second);
+        first = (first + 1) % 2;
+        second = (second + 1) % 2;
+        glVertex3f(35, -0.2, z_coord_left_first - 50 + i * 20);
+    }
+
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glPopMatrix();
+    
+     /* Druga ravan levo od glavne i njene teksture. */
+    glPushMatrix();
+    glBindTexture(GL_TEXTURE_2D, names[1]);
+    glEnable(GL_TEXTURE_2D);
+
+    glBegin(GL_QUAD_STRIP);
+    glNormal3f(0, 1, 0);
+    first = 0;
+    second = 0;
+    for (int i = 0; i <= 5; i++)
+    {
+        glTexCoord2f(first, second);
+        first = (first + 1) % 2;
+        glVertex3f(5, -0.2, z_coord_left_second - 50 + i * 20);
+
+        glTexCoord2f(first, second);
+        first = (first + 1) % 2;
+        second = (second + 1) % 2;
+        glVertex3f(35, -0.2, z_coord_left_second - 50 + i * 20);
+    }
+
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, 0);
     glPopMatrix();
 
 }
 /* Funkcija koja iscrtava loptu. */
 static void nacrtaj_loptu(){
     
-    /* Osvetljenje za loptu. */
-    GLfloat material_diffuse1[] = {0.5, 0.5, 1, 1};
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_diffuse1);
+    /* Osvetljenje za loptu. */ 
+    GLfloat material_diffuse[] = {0.3, 0.8, 0.8, 1};
+    GLfloat material_ambient[] = { 1.0, 0.1, 0.1, 1 };
+    GLfloat material_specular[] = { 0.1 , 0.1,0.1, 0.5 };
+    
+    glMaterialfv(GL_FRONT, GL_AMBIENT, material_ambient);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, material_specular);    
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_diffuse);
     
     glPushMatrix();
     glTranslatef(x_coord, y_coord + 1.6, z_coord);
@@ -316,6 +451,7 @@ static void move_objects(int id)
     
     if(restart)
     {
+        /* Resetovanje pocetih vrednosti. */
         postavi_pocetne_vrednosti();
         glutPostRedisplay();
     }
@@ -327,6 +463,13 @@ static void move_objects(int id)
     /* Pomeram koordinate obe ravni ka objektu. */
        z_plane -= brzina_lopte;
        z_plane2 -= brzina_lopte;
+       
+    /* Pomeram koordiate ravni levo i desno od glavne. */
+    z_coord_left_first -= brzina_lopte;
+    z_coord_left_second -= brzina_lopte;
+    z_coord_right_first -= brzina_lopte;
+    z_coord_right_second -= brzina_lopte;
+
 
     /* Pomeram prepreke ka objektu. */
      for (int i = 0; i < poz1; i++)
@@ -342,28 +485,44 @@ static void move_objects(int id)
     if (udesno && x_coord > -4.5)
         x_coord -= 0.2;
 
-    /* Povecava se ugao za koji se rotira loptica. */
-    ugao += 1;
-    if (ugao > 360)
-        ugao = 0;
-    
     
     /*Pocetak jedne ravni nadovezujemo na kraj druge, i tako sve u krug.*/
     if (z_plane + 50 <= 0)
     {
-        z_plane = 150;
+        z_plane = 149;
         postavi_prepreke(1);
     }
     if (z_plane2 + 50 <= 0)
     {
-        z_plane2 = 150;
+        z_plane2 = 149;
         postavi_prepreke(2);
+    }
+    
+    if (z_coord_left_first + 50 <= 0)
+    {
+        z_coord_left_first = 149;
+    }
+    if (z_coord_left_second + 50 <= 0)
+    {
+        z_coord_left_second = 149;
+    }
+
+    if (z_coord_right_first + 50 <= 0)
+    {
+        z_coord_right_first = 149;
+    }
+    if (z_coord_right_second + 50 <= 0)
+    {
+        z_coord_right_second = 149;
     }
     
     /* Rotiranje loptice. */
     ugao_rotacije += 30;
     if (ugao_rotacije >= 360)
         ugao_rotacije += -360;
+    
+    glRotated(ugao_rotacije, 1, 0, 0);
+
     
     /* Svaki put ispitujem da li je doslo  do kolizije. */
     upit_preseka();
@@ -378,7 +537,7 @@ static void move_objects(int id)
 /*Iscrtavanje prepreka. */
 static void nacrtaj_prepreke(int tip)
 {
-    int broj_prepreka = 0;
+    int broj_prepreka;
     
     if (tip == 1)
         broj_prepreka = poz1;
@@ -393,7 +552,7 @@ static void nacrtaj_prepreke(int tip)
         else
             p = prepreke_2[i];
         
-        /* Osvetljenje za preprke. */
+        /* Osvetljenje za prepreke. */
         GLfloat material_ambient[] = {0.22, 0.15, 0.05, 1.0};
         GLfloat material_diffuse[] = {0.7, 0.45, 0.2, 1.0};
         GLfloat material_specular[] = {0.4, 0.3, 0.15, 1.0};
@@ -409,7 +568,7 @@ static void nacrtaj_prepreke(int tip)
         {
             glPushMatrix();
             glTranslatef(p.x, p.y, p.z);
-            glScalef(0.6, 0.6, 0.6);
+            glScalef(0.7, 0.7, 0.7);
             glutSolidIcosahedron();
             glPopMatrix();
         }
@@ -435,9 +594,10 @@ static void postavi_prepreke(int tip)
 
     /* Po ravni cu imati 10 redova sa preprekama.
      * Po redu cu imati max 4 prepreke. */
+    int broj_prepreka;
     for (int i = 0; i < 10; i++)
     {
-        int broj_prepreka = (int)rand() % 5;
+        broj_prepreka = (int)rand() % 5;
         if (broj_prepreka == 0)
             broj_prepreka = 2;
         int dijamant = 0;
@@ -475,7 +635,7 @@ static void postavi_prepreke(int tip)
                     p.z = z_plane + 50 - i * 10;
                     prepreke_1[poz1++] = p;
                 }
-                else
+                    else
                 {
                     p.z = z_plane2 + 50 - i * 10;
                     prepreke_2[poz2++] = p;
@@ -489,9 +649,11 @@ static void postavi_prepreke(int tip)
  * vrsi pocetno iscrtavanje prepreka na drugi deo ravni 1. */
 static void postavi_prepreke_na_pocetku()
 {
+    int broj_prepreka;
+    
     for (int i = 0; i <= 2; i++)
     {
-        int broj_prepreka = (int)rand() % 5;
+        broj_prepreka = (int)rand() % 5;
         if (broj_prepreka < 4)
             broj_prepreka++;
         int dijamant = 0;
@@ -545,7 +707,7 @@ static void upit_preseka()
         for (int i = 0; i < poz1; i++)
         {
             Prepreka p = prepreke_1[i];
-            if (rastojanje_lopta_prepreka(p) < 1.22)
+            if (rastojanje_lopta_prepreka(p) < 1.5)
             {
                 /* Ako je dijamat, uvecava se skor za 1. */
                 if (prepreke_1[i].tip == 0)
@@ -598,9 +760,8 @@ static void postavi_pocetne_vrednosti(){
         start = 0;
         end = 0;
         
-        ugao = 0;
         brzina_lopte = 0.35;
-        faktor_ubrzanja = 1.0001;
+        faktor_ubrzanja = 1.0005;;
         
         ulevo = 0 ;
         udesno = 0;
@@ -626,4 +787,90 @@ static void postavi_pocetne_vrednosti(){
         
         poz1 = 0;
         poz2 = 0;
+}
+
+/* Funkcija za posatavljanje tekstura. */
+static void postavi_teksture()
+{
+    
+    Image *image;
+
+    /* Ukljucuju se teksture. */
+    glEnable(GL_TEXTURE_2D);
+
+    glTexEnvf(GL_TEXTURE_ENV,
+              GL_TEXTURE_ENV_MODE,
+              GL_REPLACE);
+
+    /*
+     * Inicijalizuje se objekat koji ce sadrzati teksture ucitane iz
+     * fajla.
+     */
+    image = image_init(0, 0);
+
+    /* Generisu se identifikatori tekstura. */
+    glGenTextures(4, names);
+    
+    /* Kreira se prva tekstura. */
+    image_read(image, ZID);
+
+    glBindTexture(GL_TEXTURE_2D, names[0]);
+    glTexParameteri(GL_TEXTURE_2D,
+                    GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D,
+                    GL_TEXTURE_WRAP_T, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D,
+                    GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,
+                    GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
+                 image->width, image->height, 0,
+                 GL_RGB, GL_UNSIGNED_BYTE, image->pixels);
+    
+   
+    /* Kreira se druga tekstura. */
+    image_read(image, VODA);
+
+    glBindTexture(GL_TEXTURE_2D, names[1]);
+    glTexParameteri(GL_TEXTURE_2D,
+                    GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D,
+                    GL_TEXTURE_WRAP_T, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D,
+                    GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,
+                    GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
+                 image->width, image->height, 0,
+                 GL_RGB, GL_UNSIGNED_BYTE, image->pixels);
+    
+
+    /* Iskljucujemo aktivnu teksturu */
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    /* Unistava se objekat za citanje tekstura iz fajla. */
+    image_done(image);
+    
+}
+
+/* Funkcija koja prikazuje tekuci rezultat. */
+static void prikazi_tekuci_rezultat(){
+    
+    /* Karakterske promenljive za rasterizaciju rezultata. */
+    char vrednost_na_ekranu[25] = "SCORE: ";
+    char rezultat[25];
+    
+    /* Koordinate od kojih pocinjem rasterizaciju. */
+    glRasterPos3f(x_coord -3, y_coord + 5, 5);
+    
+    
+    sprintf(rezultat, " %d ", score);
+    strcat(vrednost_na_ekranu, rezultat);
+
+    int n = (int)strlen(vrednost_na_ekranu);
+        
+    for (int i = 0; i < n; i++)
+    {
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, vrednost_na_ekranu[i]);
+    }   
 }
